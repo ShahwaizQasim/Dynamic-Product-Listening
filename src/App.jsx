dc8241fa10c5b8c7b8e3d6921bf9be2832badbde
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-10-02 00:34:52 -0700

    not found page add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AllProducts from './pages/allProducts'
 import ProductsDetail from './pages/productDetail'
+import NotFound from './pages/notFound'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route>
         <Route path='/' element={<AllProducts />} />
         <Route path='/ProductDetail/:id' element={<ProductsDetail />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
     </BrowserRouter>
