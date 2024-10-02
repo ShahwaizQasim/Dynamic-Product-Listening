f6a9c84daeca7c0163e3556edc0e01355e359e0f
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-10-02 00:30:35 -0700

    products detail

--- src/components/categorychip.jsx
+++ src/components/categorychip.jsx
@@ -1,12 +1,10 @@
 
-function CategoriesBox({children, isChosen}){
-
-    console.log(isChosen);
+function CategoriesBox({children, isChosen, onClick}){
     
     const categories = children;
     return(
         <>
-        <div className={`${isChosen ? 'box2': 'box'}`}>
+        <div className={`${isChosen ? 'box2': 'box'}`} onClick={onClick}>
             <h3>{categories?.name}</h3>
         </div>
         </>
--- src/pages/allProducts.jsx
+++ src/pages/allProducts.jsx
@@ -2,78 +2,89 @@ import { useEffect, useState } from "react";
 import Card from "../components/card";
 import CategoriesBox from "../components/categorychip";
 import "../index.css";
+import axios from "axios";
 
 function AllProducts() {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [chosenCategory, setChosenCategory] = useState("All");
 
   useEffect(() => {
+    console.log("useffect run");
     productsListening();
-  }, []);
-
-  const productsListening = async () => {
-    try {
-      const PRODUCT_API = await fetch("https://dummyjson.com/products");
-      const res = await PRODUCT_API.json();
-      setLoading(false);
-      setProducts([...res.products]);
-      console.log("Response", res);
-    } catch (error) {
-      console.log("error", error);
-      setLoading(false)
-    }
-  };
+  }, [chosenCategory]);
 
   useEffect(() => {
     ProductCategories();
   }, []);
+
+  const productsListening = async () => {
+    const url =
+      chosenCategory === "All"
+        ? `https://dummyjson.com/products`
+        : `https://dummyjson.com/products/categories/${chosenCategory}`;
+    // console.log("url", url);
+
+    try {
+      let res = await axios.get(url);
+      console.log('res', res);
+      setProducts([...res.data.products]);
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const ProductCategories = async () => {
     try {
-      const PRODUCT_CATEGORIES_API = await fetch(
-        "https://dummyjson.com/products/categories"
+      const categories_res = await axios.get(
+        `https://dummyjson.com/products/categories/`
       );
-      const categories_res = await PRODUCT_CATEGORIES_API.json();
-      setCategories([...categories_res]);
-      console.log("Categories Response", categories_res);
+      setCategories([...categories_res.data]);
     } catch (error) {
       console.log("Error", error.message);
     }
   };
-  // console.log("products", products);
+  console.log("products", products);
+  console.log("chosenCategory", chosenCategory);
 
   return (
     <>
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
+      {loading ? (
+        <h1 className="text-center">Loading...</h1>
+      ) : (
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              <CategoriesBox
+                onClick={() => setChosenCategory("All")}
+                isChosen={chosenCategory === "All"}
+                children={{
+                  name: "All",
+                  slug: "All",
+                }}
+              />
 
+              {categories.map((categories_data) => {
+                return (
+                  <CategoriesBox
+                    onClick={() => setChosenCategory(categories_data.slug)}
+                    key={categories_data.slug}
+                    children={categories_data}
+                    isChosen={categories_data.slug === chosenCategory}
+                  />
+                );
+              })}
 
-            <CategoriesBox
-                isChosen = {chosenCategory === "All"}
-              children={{
-                name: "All",
-                slug: "All",
-              }}
-            />
-
-            {categories.map((categories_data) => {
-              return (
-                <CategoriesBox
-                  key={categories_data.slug}
-                  children={categories_data}
-                  isChosen={categories_data.slug === chosenCategory}
-                />
-              );
-            })}
-
-            {products.map((data) => {
-              return <Card key={data.id} item={data} />;
-            })}
+              {products.map((data) => {
+                return <Card key={data.id} item={data} />;
+              })}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 }
